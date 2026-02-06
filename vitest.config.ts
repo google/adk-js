f06dcc9d40726b4a37125200dadaf96ed8cb2852
kalenkevich <kalenkevich@google.com>
2026-02-05 16:08:03 -0800

    chore: add test coverage npm command and include that to the CI check.

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,6 +12,26 @@ export default defineConfig({
     environment: 'node',
     globals: true,
     include: ['**/*_test.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json'],
+      include: ['core/src/**/*.ts', 'dev/src/**/*.ts'],
+      exclude: [
+        './dev/src/browser/**',
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/coverage/**',
+      ],
+      // Those values are from the npm run test:coverage command run on 2026-02-06
+      // and are used to ensure that the test coverage does not decrease.
+      // Once the test coverage increases, these values should be updated (manually).
+      thresholds: {
+        statements: 53,
+        branches: 76,
+        functions: 59,
+        lines: 53,
+      },
+    },
   },
   resolve: {alias: {'@google/adk': path.resolve(__dirname, './core/src')}},
 });
