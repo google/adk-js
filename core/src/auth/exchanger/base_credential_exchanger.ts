ac95b336e9772eed6cd7b0db0f881ca5f2ffd157
kalenkevich <kalenkevich@google.com>
2026-03-31 15:26:55 -0700

    feat: add auth preprocessor and update auth handler.

--- core/src/auth/exchanger/base_credential_exchanger.ts
+++ core/src/auth/exchanger/base_credential_exchanger.ts
@@ -35,6 +35,7 @@ export interface BaseCredentialExchanger {
    * @returns The exchanged credential.
    * @throws CredentialExchangeError: If credential exchange fails.
    */
+
   exchange(params: {
     authScheme?: AuthScheme;
     authCredential: AuthCredential;
