5da0b9c3d53ff3ec3176c2b7d228238f7eb39769
kalenkevich <kalenkevich@google.com>
2026-04-08 18:20:36 -0700

    license header

--- tests/integration/build_setup/js_commonjs/db_init_check.js
+++ tests/integration/build_setup/js_commonjs/db_init_check.js
@@ -1,3 +1,8 @@
+/**
+ * @license
+ * Copyright 2026 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
 const {DatabaseSessionService} = require('@google/adk'); // eslint-disable-line @typescript-eslint/no-require-imports
 const {MikroORM} = require('@mikro-orm/core'); // eslint-disable-line @typescript-eslint/no-require-imports
 
