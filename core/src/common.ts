019581df278fd792ab1d605b934bc572ea5bd9a3
Amaad Martin <amaadmartin@google.com>
2026-04-22 14:26:52 -0700

    feat: port vertex ai search tool from python

--- core/src/common.ts
+++ core/src/common.ts
@@ -199,6 +199,7 @@ export {
   PreloadMemoryTool,
 } from './tools/preload_memory_tool.js';
 export {ToolConfirmation} from './tools/tool_confirmation.js';
+export {VertexAiSearchTool} from './tools/vertex_ai_search_tool.js';
 export {LogLevel, getLogger, setLogLevel, setLogger} from './utils/logger.js';
 export type {Logger} from './utils/logger.js';
 export {isGemini2OrAbove} from './utils/model_name.js';
