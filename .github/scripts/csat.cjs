06fe3eb5ff5533e8f06613e5e6f9697719eade07
Shashank R <rshash@google.com>
2026-05-27 10:08:47 +0530

    Combine constant.cjs into csat.cjs

--- .github/scripts/csat.cjs
+++ .github/scripts/csat.cjs
@@ -4,7 +4,31 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-const CONSTANT_VALUES = require('./constant.cjs');
+const CONSTANT_VALUES = {
+  GLOBALS: {
+    LABELS: {
+      BUG: 'bug',
+      DOCUMENTATION: 'documentation',
+      GOOD_FIRST_ISSUE: 'good first issue',
+      ENHANCEMENT: 'enhancement',
+      QUESTION: 'question',
+      JAVASCRIPT: 'javascript',
+      STATUS_IN_PROGRESS: 'status/in-progress',
+      NEEDS_REVIEW: 'needs review'
+    },
+    STATE: { CLOSED: 'closed' },
+  },
+  MODULE: {
+    CSAT: {
+      YES: 'Yes',
+      NO: 'No',
+      BASE_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSeuqIP8vcNJv0Gv84ruyxmvrMQElhB2L0saRtuapK7c28QMWQ/viewform?',
+      SATISFACTION_PARAM: 'entry.2064764942=',
+      ISSUEID_PARAM: '&entry.666097176=',
+      MSG: 'Are you satisfied with the resolution of your issue?',
+    }
+  }
+};
 
 /**
  * Invoked from csat_adkjs.yml file to post survey link
